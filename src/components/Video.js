9630a951f23b9d1a612736a0e0d62726c1c3019e
Barnini Nicholas <Barnini-Nicholas@users.noreply.github.com>
2018-02-04 18:16:40 +0100

    ajout vue poce bleu poce rouge ^^^

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,12 +1,32 @@
 import React, {Component} from 'react';
 import firebase from './fire';
 import Rater from 'react-rater'
+import axios from 'axios';
 import 'react-rater/lib/react-rater.css'
 
 class Video extends Component {
 	constructor(props) {
 	    super(props)
 
+	    var viewCount;
+
+		 this.state = {
+		      viewCount: 0,
+		      likeCount: 0,
+		      dislikeCount: 0
+		}  
+
+
+		var xhr = new XMLHttpRequest();
+	    xhr.open("GET", "https://www.googleapis.com/youtube/v3/videos?part=statistics&id="+this.props.url.split('embed/')[1]+"&key=%20AIzaSyD_YtgV2h5PmZNBiZyiFeemJesSqij28Vg", false);  // synchronous request
+	    xhr.send(null);
+		var jsonparsed = JSON.parse(xhr.responseText);
+
+			 this.state = {
+			      viewCount: jsonparsed.items[0].statistics.viewCount,
+			      likeCount: jsonparsed.items[0].statistics.likeCount,
+			      dislikeCount: jsonparsed.items[0].statistics.dislikeCount
+			}  
 	}
    
 	removeVideo() {
@@ -30,7 +50,10 @@ class Video extends Component {
 	     	
 				<h5>{this.props.name}</h5>
 				<Rater total={5} rating={0} />
-                <p>{this.props.description}</p>
+				<p> Nb vues : {this.state.viewCount}</p>
+				<p> Nb poce bleu : {this.state.likeCount}</p>
+				<p> Nb poce rouge : {this.state.dislikeCount}</p>
+                <p> Description :{this.props.description}</p>
 
 				<button class="waves-effect waves-light btn" onClick={this.removeVideo.bind(this)}><i class="material-icons right">delete</i>Supprimer</button>
 
